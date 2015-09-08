28304ff30995ae5bb7cd8da797d1168aafd594fe
gus3000 <gus3000spam@gmail.com>
2015-09-08 20:28:25 +0200

    Testing single-file Model+View

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,16 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Card
+public class Card : MonoBehaviour
+{
+    [SerializeField]
+    private CardModel model;
+    [SerializeField]
+    private int test;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
+
+
+public class CardModel : MonoBehaviour
 {
     //global
     enum ColorType { Combat, Conjuring, Healing, Sorcery };
 
     //local
+    [SerializeField]
     private int id;
+    [SerializeField]
     private ColorType color;
+
     //private Effect effect;
+    [SerializeField]
     private bool isEnchantment;
+    [SerializeField]
     private string description;
-    
 }
