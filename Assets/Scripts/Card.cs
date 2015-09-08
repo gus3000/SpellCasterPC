5fc3dba73b76e96e1c2b2f21a801de31f6b454cd
gus3000 <gus3000spam@gmail.com>
2015-09-08 18:48:19 +0200

    Added base files

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card
+{
+    //global
+    enum ColorType { Combat, Conjuring, Healing, Sorcery };
+
+    //local
+    private int id;
+    private ColorType color;
+    //private Effect effect;
+    private bool isEnchantment;
+    private string description;
+    
+}
